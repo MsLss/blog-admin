6906ae395e484e8f0d100f41f14a022ae9aa155d
MsLss <2079417144@qq.com>
2020-11-24 20:48:02 +0800

    封装了api接口

---
+++ admin/src/api/category.js
@@ -0,0 +1,45 @@
+// 引入axios请求配置文件
+
+import request from '../utils/request'
+// 获取分类管理分页接口
+export function getCategoryList(name,status,current = 1,size = 10){
+    return request({
+        url:'/article/category/search',
+        method:'POST',
+        data:{
+            name,
+            status,
+            current,
+            size
+        }
+    })
+};
+// 新增分类接口
+ export function addCategoryDate(data){
+    return request({
+        url:'/article/category',
+        method:'POST',
+        data
+    })
+ }
+// 删除分类接口
+export function deleeteCategoryDate(id){
+    return request({
+        url:`/article/label/${id}`,
+        method:'DELETE',
+    })
+}
+// 查询单条分类数据接口
+export function findCategoryDate(id){
+    return request({
+        url:`/article/label/${id}`,
+        method:'GET',
+    })
+}
+// 修改分类接口
+export function updateCategoryData(){
+    return request({
+        url:"/article/category",
+        method:'PUT'
+    })
+}
